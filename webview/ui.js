dc16df048660e93650dbd10d2cb4c5f64a052e13
pica <xi_aochi@163.com>
2024-04-30 13:01:51 +0800

    button icon

--- webview/ui.js
+++ webview/ui.js
@@ -165,47 +165,47 @@ function renderMarkdown(placeholder, receivedContent) {
 function addCodeBlockFeatures(parentElement) {
     parentElement.querySelectorAll("pre code").forEach((block) => {
         hljs.highlightBlock(block);
-    
+
         const copyButton = createElement({
-            tag: 'button',
-            classes: ['copy-btn'],
+            tag: 'i',
+            classes: ['copy-btn', 'icon-btn'],
             attributes: {
-                'type': 'button'
+                'title': 'Copy'
             },
-            text: 'Copy'
+            html: '<svg data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"></path></svg>'
         });
-    
+
         copyButton.addEventListener('click', () => {
             copyTextToClipboard(block.textContent);
         });
-    
+
         block.parentNode.insertAdjacentElement("beforeend", copyButton);
-    
+
         const insertButton = createElement({
-            tag: 'button',
-            classes: ['insert-btn'],
+            tag: 'i',
+            classes: ['insert-btn', 'icon-btn'],
             attributes: {
-                'type': 'button'
+                'title': 'Insert'
             },
-            text: 'Insert To'
+            html: '<svg data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"> <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75"></path> </svg>'
         });
-    
+
         insertButton.addEventListener('click', () => {
             insertTo(block.textContent);
         });
-    
+
         block.parentNode.insertAdjacentElement("beforeend", insertButton);
-    });    
+    });
 
     // Add copy button for the div's content
     const divContent = parentElement.getAttribute('data-text');
     const divCopyButton = createElement({
-        tag: 'button',
-        classes: ['copy-btn'],
+        tag: 'i',
+        classes: ['copy-btn', 'icon-btn'],
         attributes: {
-            'type': 'button'
+            'title': 'Copy'
         },
-        text: 'Copy'
+        html: '<svg data-slot="icon" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"></path></svg>'
     });
 
     divCopyButton.addEventListener('click', () => {
@@ -215,7 +215,7 @@ function addCodeBlockFeatures(parentElement) {
     parentElement.insertAdjacentElement("beforeend", divCopyButton);
 }
 
-function insertTo(text){
+function insertTo(text) {
     vscode.postMessage({
         command: 'insert',
         text: text
